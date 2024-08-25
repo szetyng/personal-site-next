87755b558507339ed18731242e34903e99ca48cc
Sze Tyng <lee.szetyng@gmail.com>
2024-08-26 00:48:33 +0800

    minor styling

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -23,30 +23,32 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className="flex px-20 py-4">
-        <div className="w-2/5">
-          <Link href="/">
-            <h1 className="font-bold text-2xl  hover:text-off-blue">
-              sze tyng lee
-            </h1>
-          </Link>
-        </div>
-        <div className="flex w-3/5 gap-4">
-          <div className="grow"></div>
-          {navLinks.map((link) => {
-            return (
-              <Link
-                href={link.path}
-                className={`font-bold hover:text-off-blue ${
-                  pathname === link.path ? "text-off-blue" : ""
-                }`}
-                disabled={true}
-                key={link.path}
-              >
-                {link.text}
-              </Link>
-            );
-          })}
+      <nav className="flex justify-center">
+        <div className="flex py-4 md:px-20 px-8 max-w-7xl w-full">
+          <div className="w-2/5">
+            <Link href="/">
+              <h1 className="font-bold text-2xl  hover:text-off-blue">
+                sze tyng lee
+              </h1>
+            </Link>
+          </div>
+          <div className="flex w-3/5 gap-4">
+            <div className="grow"></div>
+            {navLinks.map((link) => {
+              return (
+                <Link
+                  href={link.path}
+                  className={`font-bold hover:text-off-blue ${
+                    pathname === link.path ? "text-off-blue" : ""
+                  }`}
+                  disabled={true}
+                  key={link.path}
+                >
+                  {link.text}
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </nav>
     </>
